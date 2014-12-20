89e7994aa62b8c55d910b6d09f6238ecb4df8350
Nik Butenko <nik@butenko.me>
2014-12-21 01:36:11 +1100

    Refactored feeds fetcher, cache feeds in json

--- app.js
+++ app.js
@@ -3,55 +3,131 @@
 var FeedParser = require('feedparser');
 var request = require('request');
 var fs = require('fs');
-var iconv = require('iconv-lite');
 var glob = require('glob');
 var _ = require('underscore');
-var Readable = require('stream').Readable;
+var mkdirp = require('mkdirp');
+var Iconv = require('iconv').Iconv;
 
 var feeds = _.map(glob.sync('./parsers/*.js'), require);
 
+function createDir(name, date, callback) {
+  var day = [
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate()
+  ].join('-');
+  var dir = ['cache', name, day].join('/');
 
-function updateFeed(feed) {
-	return request.get(feed.url, {encoding: 'binary'}, function (error, res) {
-		var rs = new Readable(),
-			feedparser = new FeedParser();
+  mkdirp(dir, function() {
+    callback(null, dir);
+  });
+}
 
-		if (error) {
-			return rs.emit('error', error);
-		}
-		if (res.statusCode !== 200) {
-			return rs.emit('error', new Error('Bad status code'));
-		}
+function fetch(feed) {
+  var req = request(feed.url, {timeout: 10000, pool: false});
+  req.setMaxListeners(50);
+  req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
+  req.setHeader('accept', 'text/html,application/xhtml+xml');
 
+  var feedparser = new FeedParser();
 
-		feedparser.on('error', function (error) {
-			// always handle errors
-		});
-		feedparser.on('readable', function () {
-			var stream = this,
-//		meta = this.meta,
-				itemRaw = stream.read(),
-				item,
-				content;
+  // Define our handlers
+  req.on('error', done);
+  req.on('response', function (res) {
+    if (res.statusCode !== 200) {
+      return this.emit('error', new Error('Bad status code'));
+    }
+    var charset = feed.charset || getParams(res.headers['content-type'] || '').charset;
+    res = maybeTranslate(res, charset);
+    // And boom goes the dynamite
+    res.pipe(feedparser);
+  });
 
-			while (itemRaw) {
-				content = feed.parse(itemRaw);
-				item = _.extend(_.pick(itemRaw, 'title', 'date', 'link'), {
-					image: itemRaw.enclosures.length && itemRaw.enclosures.shift().url || itemRaw.meta.image.url,
-					content: _.isString(content) && content || itemRaw.summary
-				});
-				itemRaw = stream.read();
+  feedparser.on('error', done);
+  feedparser.on('end', done);
+  feedparser.on('readable', function () {
+    while (readPost(feed, this.read())) {
 
-				// TODO: Store in DB
-				console.log(item);
-			}
-		});
-
-		rs.push(new Buffer(iconv.fromEncoding(res.body, feed.encoding), 'UTF-8'));
-		rs.push(null);
-		rs.pipe(feedparser);
-	});
+    }
+  });
 }
 
 
-_.each(feeds, updateFeed);
+function readPost(feed, itemRaw) {
+  if (!itemRaw) {
+    return null;
+  }
+
+  var item;
+  var content;
+
+  content = feed.parse(itemRaw);
+  item = _.extend(_.pick(itemRaw, 'title', 'date', 'link'), {
+    image: itemRaw.enclosures.length && itemRaw.enclosures.shift().url || itemRaw.meta.image.url,
+    content: _.isString(content) && content || itemRaw.summary
+  });
+  //itemRaw = stream.read();
+
+
+  createDir(feed.name, item.date, function(err, dir) {
+    fs.writeFile(
+      [dir, item.date.getTime() + '.json'].join('/'),
+      JSON.stringify(item, null, '\t'),
+      'UTF-8',
+      function() {
+      }
+    );
+  });
+
+  return itemRaw;
+}
+
+function maybeTranslate(res, charset) {
+  var iconv;
+  // Use iconv if its not utf8 already.
+  if (!iconv && charset && !/utf-*8/i.test(charset)) {
+    try {
+      iconv = new Iconv(charset, 'utf-8');
+      iconv.on('error', done);
+      // If we're using iconv, stream will be the output of iconv
+      // otherwise it will remain the output of request
+      res = res.pipe(iconv);
+    } catch (err) {
+      res.emit('error', err);
+    }
+  }
+  return res;
+}
+
+function getParams(str) {
+  var params = str.split(';').reduce(function (params, param) {
+    var parts = param.split('=').map(function (part) { return part.trim(); });
+    if (parts.length === 2) {
+      params[parts[0]] = parts[1];
+    }
+    return params;
+  }, {});
+  return params;
+}
+
+function done(err) {
+  if (err) {
+    console.log(err, err.stack);
+    //return process.exit(1);
+  }
+  //server.close();
+  //process.exit();
+}
+
+var server = require('http').createServer(function (req, res) {
+  var stream = require('fs').createReadStream(require('path').resolve(__dirname, '../test/feeds' + req.url));
+  res.setHeader('Content-Type', 'text/xml; charset=Windows-1251');
+  stream.pipe(res);
+
+});
+
+_.each(feeds, function (feed) {
+  server.listen(0, function () {
+    fetch(feed);
+  });
+});
--- parsers/pravda.com.ua.js
+++ parsers/pravda.com.ua.js
@@ -2,9 +2,9 @@
 
 var _ = require('underscore');
 
-
+exports.name = 'pravda.com.ua';
 exports.url = 'http://pravda.com.ua/rss';
 exports.encoding = 'win1251';
 exports.parse = function (item) {
-	return item['rss:fulltext'] && item['rss:fulltext']['#'] || '';
+  return item['rss:fulltext'] && item['rss:fulltext']['#'] || '';
 };
